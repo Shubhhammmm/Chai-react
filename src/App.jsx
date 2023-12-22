1ed451bd61b5053e99aacca9d31d62bab75bc091
Shubhhammmm <s8982034020@gmail.com>
2023-12-22 13:05:33 +0530

    added new file

---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  const[like , setLike] = useState(0)
+
+  const[dislike , setDislike] = useState(0)
+
+  const increaseValue = () => {
+    if(count < 20){
+      setCount(count + 1);
+    }else{
+      setCount(0)
+      alert("Cant go beyond 20")
+    }
+    
+  };
+
+  const decreseValue = () => {
+    if (count > 0) {
+      setCount(count - 1);
+    } else {
+      setCount(0);
+      alert(`Cant go beyond ${0}`);
+    }
+  };
+
+  const likes = () =>{
+    setLike(like + 1)
+
+  }
+
+  const dislikes = () =>{
+      setDislike(dislike + 1)
+  }
+  return (
+    <>
+      <h3 style={{color : count > 0 ? 'green' : "red"}} >Counter {count} </h3>
+      <button onClick={increaseValue}>Increase</button>
+
+      <button onClick={decreseValue}>Decrease</button>
+      <br/>
+
+      <button onClick={likes} > Like {like} </button>
+
+      <button onClick={dislikes} > Like {dislike} </button>
+    </>
+  );
+}
+
+export default App;
